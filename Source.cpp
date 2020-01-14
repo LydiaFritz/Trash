21cee986208d9be4452c88cf4d22521c4097fe8f
Lydia Fritz <Lydia.Fritz@gcu.edu>
2020-01-14 09:29:08 -0700

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,26 @@
+//Lydia Fritz
+//Lucky 7
+//This is my own work.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	//Write a program that reads in 5 integers and prints 
+	//the largest and the smallest of the group.
+
+	//create 5 ints
+	int a, b, c, d, e;
+
+	//get user input
+	cout << "Enter 5 integers: ";
+	cin >> a >> b >> c >> d >> e;
+
+	//for testing
+	cout << "The numbers are " << a << " " << b << " " << c << " " << d << " " << e << endl;
+
+
+
+	return 0;
+}
